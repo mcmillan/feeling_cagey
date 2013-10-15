7ded8c2f62c718b9a5ed32629d2af1904bf1b37a
Josh McMillan <josh@joshmcmillan.co.uk>
2013-10-15 21:50:56 +0100

    Add some docs

---
+++ README.md
@@ -0,0 +1,73 @@
+# Feeling Cagey?
+
+This is a really stupid Ruby application which pulls images with a certain tag from the Instagram API and superimposes Nicolas Cage's face onto them using OpenCV. Here's how to make it work on your machine:
+
+## Getting started
+
+Have a hearty and nutritious breakfast. There's no point in coding on an empty stomach.
+
+## Prerequisites
+
+- Ruby (I've tested it on >= 2.0.0, but in theory it should work with 1.9.x)
+- OpenCV (`brew tap homebrew/science && brew install opencv` if you're on OS X)
+- MongoDB (did you know this app is valued at $1.2bn too?)
+
+## Install the app
+
+Clone the source and run `bundle install` to install those juicy gems. After this, you're going to need to create a .env file with some configuration options in - 4 of them to be precise. Hopefully it'll look a bit like this.
+
+    INSTAGRAM_CLIENT_ID=YOUR_INSTAGRAM_CLIENT_ID
+    INSTAGRAM_CLIENT_SECRET=YOUR_INSTAGRAM_CLIENT_SECRET
+    TAG=selfie
+    MONGODB_URL="mongodb://mongodb_user:mongodb_password@your_mongodb_host:mongodb_port/mongodb_database"
+
+The app is split up into two parts, the web interface (`lib/app.rb`) and the Instagram poller (`lib/update.rb`).
+
+### Web Interface
+
+This more or less just serves up the latest media from MongoDB to /photos.json with the dimensions and positioning of the face(s) on it. We don't actually composite the photos in real time (as that would take an eternity and use a boat-load of disk space) - we just save the co-ordinates of the detected faces and render a skewed image via CSS thus allowing the app to remain light and springy, much like a nice Victoria sponge.
+
+Simply run `bundle exec rackup` and the web interface will pop up on port 9292. It's shockingly unimpressive.
+
+### Instagram Poller
+
+This is the script which does all the heavy lifting - it polls Instagram (you don't say), downloads any new media and attempts to detect faces in it. It'll wait for 10 seconds after running through all the images before hitting Instagram again in an attempt to not hit any rate limits. There's a rationale behind doing this instead of using Instagram's webhooks which I'll outline in the FAQs section on the off chance you're interested.
+
+To get this particular party started run `bundle exec rake`. It's fairly CPU intensive so I'd recommend killing it if you're not using it.
+
+## FAQs
+
+Like you've got any!
+
+### Why?
+
+Why not?
+
+### Why do you load the assets from /photos.json rather than just putting them directly on the page?
+
+In production, this application is served directly from Amazon S3 (with heavy modifications). We simply overwrite photos.json every 15 seconds from a micro instance, meaning we never have to serve any requests through the Ruby/Sinatra stack. The primary rationale behind this is that it makes this app super cheap to run and I don't have to sit here looking perplexed at 4am because I'm reading Amazon's auto scaling group documentation again.
+
+### Why don't you use Instagram's real-time callback functionality?
+
+Two reasons:
+
+- It's difficult to get it to work in a development environment (even using localtunnel et al) consistently, and I'm lazy
+- If you're subscribed to an extremely popular tag (say 'selfie') then IG will actually start hitting your application so frequently that the subsequent calls you make to their API will get you rate limited in about 10 minutes, rendering the app useless
+
+### Why aren't there any tests?
+
+It's an app with about 200 lines of code that superimposes photos of Nicolas Cage onto Instagram selfies. I'm all for TDD, but really?
+
+### Why is the code so bad?
+
+I'm drunk.
+
+## Contributing
+
+- Make a pull request
+- Shout at me via an issue or email (josh@joshmcmillan.co.uk)
+- Watch The Wicker Man
+
+## License
+
+MIT
--- app/update.rb
+++ app/update.rb
@@ -1,20 +1,31 @@
+# BIG FAT DISCLAIMER OF DREAMS:
+# This code isn't very nice. It was written at speed and this whole thing was built in about half a day.
+# Some of it doesn't make sense and should be taken with a pinch of salt. It's an app which photomanipulates Nicolas Cage, for god's sake. 
 require File.expand_path(File.dirname(__FILE__) + '/boot')
 require "fileutils"
 
+# Start afresh
 Photo.destroy_all 
 
-max_id = nil
+# We've not got any media yet, so we want to get the latest media we can find with no minimum ID
+min_id = 0
 
-(1..5).each do |depth|
-  puts "- Depth level #{depth} (we need to go deeper)"
+# I will loop you until the end of time
+#  - Beyonce, 2013
+while true
 
-  puts "-- Hitting Instagram for media tagged with #{ENV["TAG"]}, max_id #{max_id || 'not set'}"
-  media = Instagram.client.tag_recent_media(ENV["TAG"], max_id ? { max_id: max_id, count: 50 } : { count: 50 })
+  puts "-- Hitting Instagram for media tagged with #{ENV["TAG"]}, min_id #{min_id}"
+
+  # Get all the media, y0
+  media = Instagram.client.tag_recent_media(ENV["TAG"], min_id: min_id, count: 50)
+
   puts "-- #{media.count} results returned from Instagram"
 
   media.each do |m|
     puts "-- Processing media item #{m.id}"
 
+    # If we try to run a video through OpenCV all hell is going to break loose, so make sure we're only getting images from Instagram
+    # Irritatingly we can't filter this through the API call (sigh), so have to do it here.
     if m.type != 'image'
       puts "--- Media is not an image, skipping"
       next
@@ -37,18 +48,27 @@ max_id = nil
       author_picture: m.user.profile_picture
     })
 
+    # Set up the Haar Classifier using haar.xml. fullfrontal_alt.xml appears to work best from the default Haar cascades that are supplied with OpenCV.
     detector    = OpenCV::CvHaarClassifierCascade::load(File.expand_path(File.dirname(__FILE__) + '/haar.xml'))
+
+    # Define where we're going to whack this image temporarily.
+    # All Instagram images are jpegs for now (if this changes it shouldn't matter as )
     output_file = "tmp/#{p.source_id}.jpg"
+
     begin
       puts "--- Downloading image..."
-      `wget --quiet -O #{output_file} #{p.image_url}` # lol
+
+      # Download the image via wget. This is remarkably dubious and will silently fail, hence why this whole thing is wrapped in a try/catch
+      # Should definitely be refactored to at least check if we get output to stderr
+      `wget --quiet -O #{output_file} #{p.image_url}` 
 
       puts "--- Running face detection..."
       cv_image = OpenCV::CvMat.load(output_file)
       p.faces  = []
 
+      # Do all the detection and shit
       detector.detect_objects(cv_image).each do |region|
-        next if region.width < 100 # Smaller faces are more likely to be false-positives and aren't as funny
+        next if region.width < 100 # Smaller faces are more likely to be false-positives and aren't as funny, so skip them
 
         p.faces << {
           width: region.width,
@@ -57,18 +77,18 @@ max_id = nil
           left: region.top_left.x
         }
       end
-
-      File.delete(output_file)
     rescue => e
       puts "!!! Something went horrendously wrong during the download/face recognition phase - skipping"
       puts "!!! #{e}"
       next
     ensure
+      # Make sure we trash the temporarily downloaded image
       FileUtils.rm_f(output_file)
     end
 
     puts "--- #{p.faces.count} faces found"
 
+    # Don't bother saving that nice model we created up there unless there's actually any faces in the bloody thing
     next unless p.faces.any?
 
     puts "--- Saving..."
@@ -78,5 +98,8 @@ max_id = nil
 
   puts "-- All media processed"
 
-  max_id = media.pagination.next_max_id
+  # Update the min_id, have a little sleep for 10 seconds so we don't hit any rate limits (fat chance with the
+  # time it takes to process these images, however it might be a concern if you're using an obscure hashtag)
+  min_id = media.pagination.min_tag_id
+  sleep 10
 end
